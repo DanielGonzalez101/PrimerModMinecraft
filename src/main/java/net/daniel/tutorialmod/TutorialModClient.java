02a946459458a5c228a499e04029ae338e5cf945
Daniel Gonzalez Navarro <daniel@MacBook-Air-de-Daniel.local>
2025-12-07 01:51:57 -0500

    subo el proyecto

---
+++ src/main/java/net/daniel/tutorialmod/TutorialModClient.java
@@ -0,0 +1,15 @@
+package net.daniel.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * TutorialModClient
+ */
+public class TutorialModClient implements ClientModInitializer{
+
+	@Override
+	public void onInitializeClient() {
+	}
+
+  
+}
